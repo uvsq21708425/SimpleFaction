b3bd74b248fdb0001dd5c4eb34a865375e3ceba1
uvsq21708425 <astrid.leveque@ens.uvsq.fr>
2020-09-30 15:33:19 +0200

    Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,1 +1,17 @@
 
+public class Fraction
+{
+	private int num;
+	private int denum;
+	
+	public Fraction(int num, int denum)
+	{
+		this.num=num;
+		this.denum=denum;
+	}
+	
+	public String toString()
+	{
+		return num + "/" + denum;
+	}
+}
